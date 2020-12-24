56888fb1b92fa6f8b3fe8f6ef460b96a138d7bf2
Jeff Inman <jeffery.inman@hotmail.com>
2020-12-24 15:54:13 -0800

    created projectiles moving in opposite directions

--- index.js
+++ index.js
@@ -13,12 +13,34 @@ class Player {
         this.radius = radius
         this.color = color
     }
-
     draw(){
         c.beginPath()
         c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
         c.fillStyle = this.color
         c.fill()
+    }
+}
+
+
+class Projectile {
+    constructor(x, y, radius, color, velocity){
+        this.x = x
+        this.y = y
+        this.radius = radius
+        this.color = color
+        this.velocity = velocity
+    }
+    draw() {
+        c.beginPath()
+        c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+        c.fillStyle = this.color
+        c.fill()
+    }
+    
+    update() {
+        this.draw()
+        this.x = this.x + this.velocity.x
+        this.y = this.y + this.velocity.y
     }
 }
 
@@ -28,4 +50,30 @@ const y = canvas.height / 2
 const player = new Player (x, y, 30, 'blue')
 player.draw()
 
-console.log(player)
+const projectile = new Projectile(canvas.width / 2, canvas.height / 2, 5, 'red',
+ {
+    x: 1,
+    y: 1
+}
+)
+
+const projectile2 = new Projectile(canvas.width / 2, canvas.height / 2, 5, 'green',
+ {
+    x: -1,
+    y: -1
+}
+)
+
+const projectiles = [projectile, projectile2]
+
+function animate() {
+    requestAnimationFrame(animate)
+    projectiles.forEach((projectile) => {
+        projectile.update()
+    })
+}
+
+addEventListener('click', (event) => {})
+
+
+animate()
