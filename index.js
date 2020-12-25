7c7c414179a366bfeee7b88f02bd78e544952602
Jeff Inman <jeffery.inman@hotmail.com>
2020-12-24 16:14:54 -0800

    shoot projectiles from center of screen

--- index.js
+++ index.js
@@ -48,32 +48,32 @@ const x = canvas.width / 2
 const y = canvas.height / 2
 
 const player = new Player (x, y, 30, 'blue')
-player.draw()
 
-const projectile = new Projectile(canvas.width / 2, canvas.height / 2, 5, 'red',
- {
-    x: 1,
-    y: 1
-}
-)
 
-const projectile2 = new Projectile(canvas.width / 2, canvas.height / 2, 5, 'green',
- {
-    x: -1,
-    y: -1
-}
-)
 
-const projectiles = [projectile, projectile2]
+const projectiles = []
 
 function animate() {
     requestAnimationFrame(animate)
+    c.clearRect(0, 0, canvas.width, canvas.height)
+    player.draw()
     projectiles.forEach((projectile) => {
         projectile.update()
     })
 }
 
-addEventListener('click', (event) => {})
+addEventListener('click', (event) => {
+    const angle = Math.atan2(event.clientY -canvas.height / 2, event.clientX - canvas.width / 2
+        )
+
+        const velocity = {
+            x: Math.cos(angle),
+            y: Math.sin(angle)
+        }
+
+    projectiles.push(new Projectile(canvas.width / 2, canvas.height / 2, 5, 'red', velocity)
+    )
+})
 
 
 animate()
