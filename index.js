4934561512cab5dc9959e35fe35c9738977dc678
Jeff Inman <jeffery.inman@hotmail.com>
2020-12-24 20:08:34 -0800

    shrink enemies on hit

--- index.js
+++ index.js
@@ -130,16 +130,27 @@ function animate() {
             cancelAnimationFrame(animationID)
         }
         projectiles.forEach((projectile, projectileIndex) => {
-            const dist = Math.hypot(projectile.x - enemy.x, projectile.y - enemy.y)
+                const dist = Math.hypot(projectile.x - enemy.x, projectile.y - enemy.y)
 
-            //objects touch
-           if (dist - enemy.radius - projectile.radius < 1) {
-               setTimeout (() => {
-                   enemies.splice(index, 1)
-                   projectiles.splice(projectileIndex, 1)
-               }, 0)
-           
-           }
+                // when projectiles touch enemy
+                if (dist - enemy.radius - projectile.radius < 1) {
+                    if (enemy.radius - 10 > 5) {
+                       gsap.to(enemy, {
+                           radius: enemy.radius - 10
+                       })
+                       setTimeout(() => {
+                        
+                        projectiles.splice(projectileIndex, 1)
+                    }, 0)
+                    } else {
+                        setTimeout(() => {
+                            enemies.splice(index, 1)
+                            projectiles.splice(projectileIndex, 1)
+                        }, 0)
+
+                    }  
+                }      
+                   
         })
 
     })
